7d619fc21bb2e5fda2a8554c9f792fd34dab9bb1
Dmitry Pozdnyakov <hellformenow@gmail.com>
2023-03-05 22:15:07 +0100

    Added tasl1

---
+++ Hometasks/HomeTask4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Hometasks;
+
+internal class HomeTask4
+{
+	public static void Task1()
+	{
+		Console.Write("Write the sentence, that contain word 'test' and numbers: ");
+		string textstart;
+		textstart = Console.ReadLine();
+		textstart = textstart.Replace("test", "testing");
+        Console.Write("The result is: ");
+        for (int i = 0; i < textstart.Length; i++)
+			if (char.IsDigit(textstart[i]))
+
+			{
+				Console.Write("");
+			}
+		else
+			Console.Write(textstart[i]);
+	}
+}
--- Hometasks/Program.cs
+++ Hometasks/Program.cs
@@ -5,17 +5,18 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        HomeTask1.Task1();
-        HomeTask2.Task1();
-        HomeTask2.Task2();
-        HomeTask2.Task3();
-        HomeTask2.Task4v1();
-        HomeTask2.Task4v2();
-        HomeTask3.Task1();
-        HomeTask3.Task2();
-        HomeTask3.Task3();
-        HomeTask3.Task4();
-        HomeTask3.Task5();
-        HomeTask3.Task6();
+        //HomeTask1.Task1();
+        //HomeTask2.Task1();
+        //HomeTask2.Task2();
+        //HomeTask2.Task3();
+        //HomeTask2.Task4v1();
+        //HomeTask2.Task4v2();
+        //HomeTask3.Task1();
+        //HomeTask3.Task2();
+        //HomeTask3.Task3();
+        //HomeTask3.Task4();
+        //HomeTask3.Task5();
+        //HomeTask3.Task6();
+        HomeTask4.Task1();
     }
 }
