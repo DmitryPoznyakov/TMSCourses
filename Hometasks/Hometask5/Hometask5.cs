72a3466173788d8b8d1e68e63fc018a623c93566
Dmitry Pozdnyakov <hellformenow@gmail.com>
2023-03-10 15:03:43 +0100

    Added task1 - hometask5

---
+++ Hometasks/Hometask5/Hometask5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Hometasks.Hometask5
+{
+	//Task1
+
+	internal class Hometask5
+	{
+		static void Main(string[] args)
+		{
+			Phone iPhone = new Phone(2020327, "iPhone 14", 172);
+			Phone Galaxy = new Phone(7788, "Galaxy S23", 168);
+			Phone Pixiel = new Phone(911, "Google Piexel 7", 197);
+
+			Console.WriteLine("Presented SmartPhones: ");
+			Galaxy.GetInfo();
+			Pixiel.GetInfo();
+			iPhone.GetInfo();
+
+			iPhone.receiveCall("Carl");
+			iPhone.getNumber();
+			Console.WriteLine();
+
+			Galaxy.receiveCall("Valera");
+			Galaxy.getNumber();
+            Console.WriteLine();
+
+            Pixiel.receiveCall("Vasiliy");
+			Pixiel.getNumber();
+            Console.WriteLine();
+
+            Pixiel.receiveCall("Boris",666);
+
+			Pixiel.sendMessage(1212121212,2323232,343434,4222323232323232);
+		}
+	}
+
+	//Task2
+}
+
