bad33d06302ba9175f3fa99edc24bfec932f22a6
Dmitry Pozdnyakov <hellformenow@gmail.com>
2023-03-10 15:38:07 +0100

    Done

--- Hometasks/Hometask5/Hometask5.cs
+++ Hometasks/Hometask5/Hometask5.cs
@@ -13,32 +13,32 @@ namespace Hometasks.Hometask5
 
             //Task1
 
-            //	Phone iPhone = new Phone(2020327, "iPhone 14", 172);
-            //	Phone Galaxy = new Phone(7788, "Galaxy S23", 168);
-            //	Phone Pixiel = new Phone(911, "Google Piexel 7", 197);
+            Phone iPhone = new Phone(2020327, "iPhone 14", 172);
+            Phone Galaxy = new Phone(7788, "Galaxy S23", 168);
+            Phone Pixiel = new Phone(911, "Google Piexel 7", 197);
 
-            //	Console.WriteLine("Presented SmartPhones: ");
-            //	Galaxy.GetInfo();
-            //	Pixiel.GetInfo();
-            //	iPhone.GetInfo();
+            Console.WriteLine("Presented SmartPhones: ");
+            Galaxy.GetInfo();
+            Pixiel.GetInfo();
+            iPhone.GetInfo();
 
-            //	iPhone.receiveCall("Carl");
-            //	iPhone.getNumber();
-            //	Console.WriteLine();
+            iPhone.receiveCall("Carl");
+            iPhone.getNumber();
+            Console.WriteLine();
 
-            //	Galaxy.receiveCall("Valera");
-            //	Galaxy.getNumber();
-            //          Console.WriteLine();
+            Galaxy.receiveCall("Valera");
+            Galaxy.getNumber();
+            Console.WriteLine();
 
-            //          Pixiel.receiveCall("Vasiliy");
-            //	Pixiel.getNumber();
-            //          Console.WriteLine();
+            Pixiel.receiveCall("Vasiliy");
+            Pixiel.getNumber();
+            Console.WriteLine();
 
-            //          Pixiel.receiveCall("Boris",666);
+            Pixiel.receiveCall("Boris", 666);
 
-            //	Pixiel.sendMessage(1212121212,2323232,343434,4222323232323232);
+            Pixiel.sendMessage(1212121212, 2323232, 343434, 4222323232323232);
 
-            //Task1
+            //Task2
 
             CreditCard visa = new CreditCard("1111-2222-3333-4444", 5000);
             CreditCard MasterCard = new CreditCard("4444-3333-2222-1111", 150000);
